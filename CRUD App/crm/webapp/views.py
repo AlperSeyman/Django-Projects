ed3f9fd9513d3659b91f11cef3df781ac2b63040
Alper Seyman <alperseyman@gmail.com>
2025-06-30 13:49:31 +0200

    register, login, and logout views and templates created

---
+++ CRUD App/crm/webapp/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
+
+from django.contrib.auth import aauthenticate, login, logout
+
+# Create your views here.
+
+def home(request):
+    
+    return render(request, template_name='webapp/home.html')
+
+
+
+
+# Register
+def register(request):
+    
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('login')
+
+    return render(request, template_name='webapp/register.html', context={'form':form})
+
+
+
+# Login
+def login_view(request):
+    
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+
+            # username = request.POST.get('username')
+            # password = request.POST.get('password')
+            # user = authenticate(request, username=username, password=password)
+            # if user is not None:
+                # login(request, user)
+ 
+            user = form.get_user()  # Get the logged-in user
+            login(request, user)   # Log them in
+
+            # return redirect('')
+
+    
+    return render(request, template_name='webapp/login.html', context={'form':form})
+
+
+# Logout
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+
+# Dashboard
