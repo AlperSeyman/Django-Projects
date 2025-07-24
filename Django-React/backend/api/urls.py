1a0bf5e56a415356c7da129f9c7f59391d123291
Alper Seyman <alperseyman@gmail.com>
2025-07-24 16:25:35 +0200

    created serializer and configured url

---
+++ Django-React/backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+urlpatterns = [
+    path('user/register/', CreateUserView.as_view(), name='create_user'),
+    path('token/', TokenObtainPairView.as_view(), name="get_token"),
+    path('token/refresh/', TokenRefreshView.as_view(), name="refresh_token"),
+    
+]
