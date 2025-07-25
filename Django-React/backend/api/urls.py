5b4c8fb648ce85e1df7575f1dbfe2b586b4e3421
Alper Seyman <alperseyman@gmail.com>
2025-07-25 22:40:21 +0200

    create, read, and delete's views added

--- Django-React/backend/api/urls.py
+++ Django-React/backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView
+from .views import CreateUserView, NotesListCreate, NoteDelete
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('user/register/', CreateUserView.as_view(), name='create_user'),
     path('token/', TokenObtainPairView.as_view(), name="get_token"),
     path('token/refresh/', TokenRefreshView.as_view(), name="refresh_token"),
+    path('notes/', NotesListCreate.as_view(), name="note-list"),
+    path('notes/delete/<int:pk>', NoteDelete.as_view(), name="delete-note")
     
 ]
--- Django-React/backend/api/views.py
+++ Django-React/backend/api/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 
 class CreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+    queryset = User.objects.all()           # get all users (used internally by the view)
+    serializer_class = UserSerializer       # convert User instances to/from JSON and validate data
+    permission_classes = [AllowAny]         # allow anyone to access (no login required)
 
 
+class NotesListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer       # serialize notes to/from JSON
+    permission_classes = [IsAuthenticated]  # user must be logged in 
 
+    def get_queryset(self):
+        user = self.request.user                 # Get the current logged-in user from the request
+        return Note.objects.filter(author=user)  # Return only notes where author is this user
+    
+
+    def perform_create(self, serializer):
+        # Save the new Note object with the 'author' field set to the currently logged-in user
+        # We tell the serializer to save the Note and automatically assign this user as the author
+        # self.request.user is the user who made the request (the logged-in user)
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
